935fb347381665b3f9a2262f7df5c220f6355c8c
riyuzenn <riyuzenn@gmail.com>
2023-07-07 22:54:23 +0800

    format buffer

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,19 +1,22 @@
+use std::io;
 use tokio::io::{
     AsyncRead,
     AsyncReadExt,
-    AsyncWrite, 
-    AsyncWriteExt
+    AsyncWrite,
+    AsyncWriteExt,
 };
-use std::io;
+
 // Set the default buffer capacity to 8k
 pub const BUFFER: usize = 8 * 1024;
 
-
-pub async fn read_stream<I>(stream: &mut I, buff: &mut [u8]) -> io::Result<Vec<u8>>
+pub async fn read_stream<I>(
+    stream: &mut I,
+    buff: &mut [u8],
+) -> io::Result<Vec<u8>>
 where
     I: AsyncRead + AsyncWrite + Unpin,
 {
-    let mut data = Vec::new(); 
+    let mut data = Vec::new();
     while let Ok(n) = stream.read(buff).await {
         if n == 0 {
             stream.shutdown().await?;

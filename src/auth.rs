6a4630fa65df1305cbd3e9e0e5e8f34e67b16544
riyuzenn <riyuzenn@gmail.com>
2023-07-07 23:04:41 +0800

    add copyright notice

--- src/auth.rs
+++ src/auth.rs
@@ -1,4 +1,26 @@
-use serde::{Deserialize, Serialize};
+/*
+ * Copyright (c) 2023-present repelDB
+ *
+ * See the license file for more info
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use serde::{
+    Deserialize,
+    Serialize,
+};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum UserPermission {
@@ -24,7 +46,10 @@ impl UserPermission {
         }
     }
 
-    pub fn compare(&self, o: &UserPermission) -> bool {
+    pub fn compare(
+        &self,
+        o: &UserPermission,
+    ) -> bool {
         match self {
             UserPermission::Read => matches!(o, UserPermission::Read),
             UserPermission::Write => matches!(o, UserPermission::Write),
